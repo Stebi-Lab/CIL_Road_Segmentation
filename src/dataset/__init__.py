756555fb6cb04a61a83912e055e991c62d796736
ccaspar <ccaspar@student.ethz.ch>
2024-07-07 17:46:24 +0200

    clean up

--- src/dataset/__init__.py
+++ src/dataset/__init__.py
@@ -1,4 +1,4 @@
 from src.dataset.kaeggle_dataset import KaeggleDataset
-from src.dataset.test_dataset import TestDataset
+from src.dataset.nothing_dataset import TestDataset
 
 datasetMappingDict = {"TestDataset": TestDataset,'KaeggleDataset': KaeggleDataset}
--- src/model/__init__.py
+++ src/model/__init__.py
@@ -1,4 +1,4 @@
 from src.model.simple_unet import UnetModel
-from src.model.test_model import TestModel
+from src.model.nothing_model import TestModel
 
 modelMappingDict = {"TestModel": TestModel,"UnetModel": UnetModel}
