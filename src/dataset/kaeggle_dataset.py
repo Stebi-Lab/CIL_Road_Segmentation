312feb5eff54940112a49fc6f59ff5eec17b5e4a
ccaspar <ccaspar@student.ethz.ch>
2024-07-23 20:27:46 +0200

    bug fixing

--- src/dataset/kaeggle_dataset.py
+++ src/dataset/kaeggle_dataset.py
@@ -1,12 +1,9 @@
 import os
 from typing import Any, Dict
 import random
-import numpy as np
-import torch
 from PIL import Image
 from torchvision import transforms
 from torchvision.transforms import functional as F
-
 
 from src.base.base_torch_dataset import BaseTorchDataset
 
@@ -22,10 +19,13 @@ class KaeggleDataset(BaseTorchDataset):
         self.entity_name = self.config["entity_name"] if "entity_name" in self.config.keys() and self.config[
             "entity_name"] is not None else "Kaeggle-Dataset"
         self.dataset_path = self.config["dataset_path"] if "dataset_path" in self.config.keys() else None
-        self.preloadAll = self.config["preload_all"] if "preload_all" in self.config.keys() and self.config[
-            "preload_all"] is not None else False
         self.padding = self.config["padding"] if "padding" in self.config.keys() and self.config[
             "padding"] is not None else [0, 0, 0, 0]
+        self.type = self.config["type"] if "type" in self.config.keys() and self.config[
+            "type"] is not None else 'train'
+        self.augment = self.config["augment"] if "augment" in self.config.keys() and self.config[
+            "augment"] is not None else False
+
         if self.verbose: print("Padding with {}".format(tuple(self.padding)))
 
         self.device = 'cpu'
@@ -33,13 +33,9 @@ class KaeggleDataset(BaseTorchDataset):
         self.data = []
         self.num_samples = 0
 
-        self.type = self.config["type"] if "type" in self.config.keys() and self.config[
-            "type"] is not None else 'train'
-        self.augment = self.config["augment"] if "augment" in self.config.keys() and self.config[
-            "augment"] is not None else False
-
-        print('self.augment is:', self.augment)
-        print('self.type is:', self.type)
+        if self.verbose:
+            print('self.augment is:', self.augment)
+            print('self.type is:', self.type)
 
         self.sample_transforms = transforms.Compose([
             # transforms.Resize((256, 256)),
@@ -61,67 +57,32 @@ class KaeggleDataset(BaseTorchDataset):
             samples = sorted([name for name in os.listdir(imgs_path) if '.png' in name])
             num_samples = len(samples)
             if self.verbose: print("Found {} samples at path: {}".format(num_samples, imgs_path))
-            if self.preloadAll:
-                for image in samples:
-                    self.data.append(self.load_single_img(imgs_path + "/" + image))
-            else:
-                self.data = [imgs_path + "/" + file for file in samples]
+
+            self.data = [imgs_path + "/" + file for file in samples]
 
             if (self.type != 'test') and os.path.exists(labels_path):
                 labels = sorted([name for name in os.listdir(labels_path) if '.png' in name])
                 if len(labels) != num_samples: raise Exception("Number of labels does not match number of samples")
-                if self.preloadAll:
-                    for image in labels:
-                        self.targets.append(self.load_single_img_label(labels_path + "/" + image))
-                else:
-                    self.targets = [labels_path + "/" + file for file in labels]
+
+                self.targets = [labels_path + "/" + file for file in labels]
 
             self.num_samples = num_samples
 
         if self.verbose:
             print(f"Loaded dataset {self.entity_name} with {len(self)} samples")
-            if self.preloadAll:
-                if self.type == 'test':
-                    print(f"with shape {self.data[0].shape}")
-                else:
-                    print(f"with shape {self.data[0].shape} and labels with shape {self.targets[0].shape}")
-        self.half_precision = self.config["half_precision"] if "half_precision" in self.config.keys() and self.config[
-            "half_precision"] is not None else False
-
-        # if self.half_precision:
-        #     self.data = [s.astype(np.float16) for s in self.data]
-        # else:
-        #     self.data = [s.astype(np.float32) for s in self.data]
 
     def to_device(self, device):
         self.device = device
-        if self.preloadAll:
-            self.data = [t.to(device) for t in self.data]
-            self.targets = [t.to(device) for t in self.targets]
 
     def __len__(self):
         return self.num_samples
 
     def __getitem__(self, idx):
-        if self.preloadAll:
-            if self.type != 'test':
-                return self.data[idx], self.targets[idx]
-            else:
-                return self.data[idx]
-
         if self.type != 'test':
             img, label = self.load_both(idx)
-            return (self.data[idx].split('/')[-1], img.to(self.device), label.to(self.device))
-
-        if self.type == 'test':
+            return self.data[idx].split('/')[-1], img.to(self.device), label.to(self.device)
+        else:
             return self.data[idx].split('/')[-1], self.load_single_img(self.data[idx]).to(self.device)
-
-    # def load_single(self, idx):
-    #     if (self.type != 'test'):
-    #         return (self.data[idx].split('/')[-1], self.load_single_img(self.data[idx]).to(self.device), self.load_single_img_label(self.targets[idx]).to(self.device))
-
-    #     elif (self.type == 'test'):
-    #         return (self.data[idx].split('/')[-1], self.load_single_img(self.data[idx]).to(self.device))
 
     def load_single_img(self, file_path):
         image = Image.open(file_path).convert("RGB")
@@ -129,14 +90,6 @@ class KaeggleDataset(BaseTorchDataset):
         if self.sample_transforms:
             image = self.sample_transforms(image)
         return image
-
-    # def load_single_img_label(self, file_path):
-    #     image = Image.open(file_path).convert("L")
-
-    #     if self.label_transforms:
-    #         image = self.label_transforms(image)
-    #     return image
-
 
     def load_both(self, idx):
         """
@@ -154,16 +107,16 @@ class KaeggleDataset(BaseTorchDataset):
         if self.label_transforms:
             label = self.label_transforms(label)
 
-        ## Save for visualization
-        #self.save_img_label_png(idx, image, label, dir='before_augment')
+        # Save for visualization
+        # self.save_img_label_png(idx, image, label, dir='before_augment')
 
         # Apply augmentations
         if self.augment and self.type == 'train':
             severity = 0.4  # Adjust severity as needed
             image, label = self.random_augmentations(image, label, severity)
 
-        ## Save for visualization
-        #self.save_img_label_png(idx, image, label, dir='after_augment')
+        # Save for visualization
+        # self.save_img_label_png(idx, image, label, dir='after_augment')
 
         return image, label
 
@@ -185,12 +138,12 @@ class KaeggleDataset(BaseTorchDataset):
         label = F.rotate(label, angle)
 
         # Random zoom and crop
-        scale = random.uniform(1-severity, 1+severity)
+        scale = random.uniform(1 - severity, 1 + severity)
         i, j, h, w = transforms.RandomResizedCrop.get_params(
-            image, scale=(scale, scale), ratio=(1.0, 1.0)
+            image, scale=[scale, scale], ratio=[1.0, 1.0]
         )
-        image = F.resized_crop(image, i, j, h, w, size=(image.shape[1], image.shape[2]))
-        label = F.resized_crop(label, i, j, h, w, size=(label.shape[1], label.shape[2]))
+        image = F.resized_crop(image, i, j, h, w, size=[image.shape[1], image.shape[2]])
+        label = F.resized_crop(label, i, j, h, w, size=[label.shape[1], label.shape[2]])
 
         return image, label
 
@@ -212,5 +165,3 @@ class KaeggleDataset(BaseTorchDataset):
 
         # Save the image
         combined_img.save(os.path.join(dir, f'{idx}.png'))
-
-
