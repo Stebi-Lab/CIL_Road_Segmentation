26d93fd417658f62f52d7a921acad11d6c1e7caa
athamilma <athamilma@student.ethz.ch>
2024-07-24 12:36:27 +0000

    Replace __init__.py

--- src/model/__init__.py
+++ src/model/__init__.py
@@ -2,5 +2,6 @@ from src.model.seg_former import SegFormerImpl
 from src.model.simple_unet import UnetModel
 from src.model.nothing_model import TestModel
 from src.model.unet_plusplus import UNetPlusPlusModel
+from src.model.unet_plusplus_pretrained import UNetPlusPlusModel_Pretrained
 
-modelMappingDict = {"TestModel": TestModel,"UnetModel": UnetModel, "SegFormer": SegFormerImpl, "UnetPlusPlusModel": UNetPlusPlusModel}
+modelMappingDict = {"TestModel": TestModel,"UnetModel": UnetModel, "SegFormer": SegFormerImpl, "UnetPlusPlusModel": UNetPlusPlusModel, "UnetPlusPlusModel_Pretrained": UNetPlusPlusModel_Pretrained}
